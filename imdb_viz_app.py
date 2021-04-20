5c242337dba0b6c0e262b6b8c51ea772b0526abb
Ashish Kayastha <ashishk.48@gmail.com>
2021-04-20 00:43:59 -0400

    Added a server variable for production environment.

--- imdb_viz_app.py
+++ imdb_viz_app.py
@@ -13,6 +13,7 @@ import textwrap
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 NO_DATA_GRAPH = {
     'data': [],
